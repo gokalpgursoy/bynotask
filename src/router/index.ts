bedad53b527a3f4b61bb90c30b6ab5a5748f9a4e
Gökalp Gürsoy <gokalp.gursoy@bilgeadam.com>
2020-03-24 22:07:59 +0300

    Default route added.

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,6 +11,7 @@ const routes = [
     component: Home,
     // component: () => import(/* webpackChunkName: "about" */ '../views/Home.vue')
   },
+  { path: '*', redirect: '/' },
 ]
 
 const router = new VueRouter({
