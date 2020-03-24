802a2a11b2cbb7aaa36bc414b45ac2a3e043ee86
Gökalp Gürsoy <gokalp.gursoy@bilgeadam.com>
2020-03-25 00:04:19 +0300

    Card component development started.

--- src/components/card/card.vue
+++ src/components/card/card.vue
@@ -1,6 +1,35 @@
 <template>
-  <div>
-    <h1>card</h1>
+  <div class="card">
+    <img class="card-img-top" src="https://picsum.photos/id/0/5616/3744" alt="Card image cap" />
+    <div class="card-body text-center">
+      <h4 class="card-title">Author: Alejandro Escamilla</h4>
+      <div class="row">
+        <div class="col-md-6 my-1">
+          <a
+            href="https://picsum.photos/id/1/5616/3744"
+            class="btn btn-sm btn-dark btn-block"
+            target="_blank"
+          >Download</a>
+        </div>
+        <div class="col-md-6 my-1">
+          <a
+            href="https://unsplash.com/photos/LNRyGwIJr5c"
+            class="btn btn-sm btn-info btn-block"
+            target="_blank"
+          >Visit</a>
+        </div>
+      </div>
+    </div>
+    <div class="card-footer text-center">
+      <div class="row">
+        <div class="col-md-6">
+          <small class="text-muted">Height: 3744px</small>
+        </div>
+        <div class="col-md-6">
+          <small class="text-muted">Width: 5616px</small>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,34 @@
 <template>
   <div class="home">
-
+    <div class="container py-2">
+      <div class="card-deck mb-2">
+        <card-component></card-component>
+        <card-component></card-component>
+        <card-component></card-component>
+      </div>
+      <div class="card-deck">
+        <card-component></card-component>
+        <card-component></card-component>
+        <card-component></card-component>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import card from "@/components/card/card";
 
 export default {
-  name: 'Home',
-}
+  name: "Home",
+  components: {
+    "card-component": card
+  }
+};
 </script>
 
 <style lang="sass" scoped>
 @import  '../assets/sass/variables.sass'
 
 .home
-  color: purple
   height: 100vh - ($header-height + $footer-height)
 </style>
