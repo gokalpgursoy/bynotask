c675c800cf4c323aafb8c6052860d712a29336ce
Gökalp Gürsoy <gokalp.gursoy@bilgeadam.com>
2020-03-25 21:01:29 +0300

    Card styles fixed, fontawesome added and changeItemCount() method added.

--- src/components/card/card.vue
+++ src/components/card/card.vue
@@ -1,37 +1,30 @@
 <template>
-  <div class="card" :id="id">
-    <div class="size-box">
-      <small>{{width}} x {{height}}</small>
+  <div class="card mb-3" :id="id">
+    <div class="card-header bg-dark text-light text-center">
+      <h6>Author: {{author}}</h6>
     </div>
     <img class="card-img-top" :src="downloadUrl" alt="Card image cap" />
     <div class="card-body text-center p-0 py-2 px-2">
       <div class="row">
-        <div class="col-md-6 my-1">
+        <div class="col-md-6 col-lg-3 offset-lg-3 my-1">
           <a
             :href="downloadUrl"
             class="btn btn-sm btn-dark btn-block"
             target="_blank"
-          >Download</a>
+            title="Download"
+          >
+            <i class="fa fa-download"></i>
+          </a>
         </div>
-        <div class="col-md-6 my-1">
-          <a
-            :href="url"
-            class="btn btn-sm btn-info btn-block"
-            target="_blank"
-          >Visit</a>
+        <div class="col-md-6 col-lg-3 my-1">
+          <a :href="url" class="btn btn-sm btn-info btn-block" target="_blank" title="Visit">
+            <i class="fa fa-eye"></i>
+          </a>
         </div>
       </div>
     </div>
     <div class="card-footer text-center p-0">
-      <small class="text-muted">Author: {{author}}</small>
-      <!-- <div class="row">
-        <div class="col-md-6">
-          <small class="text-muted">Width: {{height}}</small>
-        </div>
-        <div class="col-md-6">
-          <small class="text-muted">Height: {{width}}</small>
-        </div>
-      </div> -->
+      <small>{{width}} x {{height}}</small>
     </div>
   </div>
 </template>
@@ -45,23 +38,23 @@ export default {
     width: Number,
     height: Number,
     url: String,
-    downloadUrl: String,
+    downloadUrl: String
   }
 };
 </script>
 
 <style lang="sass" scoped>
-  .size-box
-    position: absolute
-    color: white
-    width: 100%
-    background-color: #343a40
-    text-align: center
-    border-bottom: 1px solid white
+.size-box
+  position: absolute
+  color: white
+  width: 100%
+  background-color: #343a40
+  text-align: center
+  border-bottom: 1px solid white
     // padding-bottom: 5px
   img
     background-color: #f7f7f7
-    height: 200px
+    height: 300px
     object-fit: contain
   sizes
     position: absolute
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,44 @@
 <template>
   <div class="home">
     <div class="container py-2">
-      <div class="card-deck mb-2" v-for="(row,i) in rowCount" :key="row">
-        <template v-for="item in items.slice((i*itemCount), (i*itemCount + itemCount))">
-          <card-component
-            :key="item.id"
-            :id="item.id"
-            :author="item.author"
-            :height="item.height"
-            :width="item.width"
-            :url="item.url"
-            :downloadUrl="item.download_url"
-          ></card-component>
-        </template>
+      <div class="row mb-2 d-none d-md-block">
+        <div class="col-md-3 offset-md-9 d-flex justify-content-end">
+          <div class="btn-group" role="group" aria-label="Basic example">
+            <button type="button" class="btn btn-dark" @click="changeItemCount(1)">
+              <i class="fas fa-bars"></i>
+            </button>
+            <button type="button" class="btn btn-dark" @click="changeItemCount(2)">
+              <i class="fa fa-columns"></i>
+            </button>
+            <button type="button" class="btn btn-dark" @click="changeItemCount(3)">
+              <i class="fas fa-ellipsis-h"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+          <div class="card-deck mb-2" v-for="(row,i) in rowCount" :key="row">
+            <template v-for="item in items.slice((i*itemCount), (i*itemCount + itemCount))">
+              <card-component
+                :key="item.id"
+                :id="item.id"
+                :author="item.author"
+                :height="item.height"
+                :width="item.width"
+                :url="item.url"
+                :downloadUrl="item.download_url"
+              ></card-component>
+            </template>
+          </div>
+        </div>
+      </div>
+      <div class="row d-flex justify-content-center mt-2 mb-5">
+        <div class="col-md-3">
+          <button class="btn btn-block" @click="getData(Number(page) + 1, limit)">
+            <i class="fas fa-chevron-down"></i>
+          </button>
+        </div>
       </div>
     </div>
   </div>
@@ -43,22 +69,28 @@ export default {
       await this.getData(this.page, this.limit);
     }
   },
-  watch: {
-    async $route() {
-      const { page } = this.$route.params;
-      if (!isNaN(page)) {
-        this.page = page;
-        await this.getData(this.page, this.limit);
-      }
-    }
-  },
+  // watch: {
+  //   async $route() {
+  //     const { page } = this.$route.params;
+  //     console.log('PAGE GG', page);
+  //     if (!isNaN(page)) {
+  //       this.page = page;
+  //       await this.getData(this.page, this.limit);
+  //     }
+  //   }
+  // },
   methods: {
     async getData(page, limit) {
+      this.page = page;
       const response = await axios.get(
         `https://picsum.photos/v2/list?page=${page}&limit=${limit}`
       );
-      this.items = response.data;
-      this.rowCount = Math.ceil(response.data.length / this.itemCount);
+      this.items = [...this.items, ...response.data];
+      this.rowCount = Math.ceil(this.items.length / this.itemCount);
+    },
+    changeItemCount(count) {
+      this.itemCount = count;
+      this.rowCount = Math.ceil(this.items.length / this.itemCount);
     }
   }
 };
